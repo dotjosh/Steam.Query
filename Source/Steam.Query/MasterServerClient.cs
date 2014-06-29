18627f0fadbead06192ae1d5ca5a71fd80e344c4
Josh Schwartzberg <dotjosh@gmail.com>
2014-06-29 19:35:59 -0400

    Initial commit

---
+++ Source/Steam.Query/MasterServerClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Steam.Query
+{
+    public class MasterServerClient
+    {
+        private const string FIRST_AND_LAST_SERVER = "0.0.0.0:0";
+        private const int HEADER_BYTES_LENGTH = 6;
+        private readonly IPAddress _steamSteamIpAddress;
+        private readonly int _steamSteamPort;
+
+        public MasterServerClient()
+            : this("hl2master.steampowered.com", 27011)
+        {
+        }
+
+        public MasterServerClient(string hostname, int steamPort)
+        {
+            _steamSteamIpAddress = Dns.GetHostEntry(hostname).AddressList[0];
+            _steamSteamPort = steamPort;
+        }
+
+        public async Task<IEnumerable<string>> GetServers(params MasterServerFilter[] masterServerFilters)
+        {
+            var servers = new List<string>();
+
+            using (var client = new UdpClient(new IPEndPoint(IPAddress.Any, 0)))
+            {
+                client.Connect(_steamSteamIpAddress, _steamSteamPort);
+
+                string thisServer = null;
+                while (thisServer != FIRST_AND_LAST_SERVER)
+                {
+                    var requestPacket = CreateRequestPacket(thisServer ?? FIRST_AND_LAST_SERVER, masterServerFilters);
+                    await client.SendAsync(requestPacket, requestPacket.Length);
+                    var response = await client.ReceiveAsync();
+                    var responseData = response.Buffer.ToList();
+                    for (int i = HEADER_BYTES_LENGTH; i < responseData.Count; i++)
+                    {
+                        var ip = string.Join(".", responseData.GetRange(i, 4).ToArray());
+                        var port = Convert.ToString(responseData[i + 4] << 8 | responseData[i + 5]);
+                        thisServer = string.Format("{0}:{1}", ip, port);
+                        if (thisServer != FIRST_AND_LAST_SERVER)
+                        {
+                            servers.Add(thisServer);
+                        }
+                        i += 5;
+                    }
+                }
+            }
+
+            return servers;
+        }
+
+        private static byte[] CreateRequestPacket(string ipAddress, IEnumerable<MasterServerFilter> filters)
+        {
+            var buffer = new List<byte> { 0x31, 0xFF };
+            buffer.AddRange(System.Text.Encoding.ASCII.GetBytes(ipAddress));
+            buffer.Add(0x00);
+            var filtersString = string.Join("", filters.Select(x => x.Key + "\\" + x.Value));
+            buffer.AddRange(System.Text.Encoding.ASCII.GetBytes(filtersString));
+            return buffer.ToArray();
+        }
+    }
+}
---
+++ Source/Steam.Query/MasterServerFilter.cs
@@ -0,0 +1,19 @@
+﻿namespace Steam.Query
+{
+    public class MasterServerFilter
+    {
+        public MasterServerFilter(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+
+        public static MasterServerFilter Gamedir(string value)
+        {
+            return new MasterServerFilter("gamedir", value);
+        }
+    }
+}
