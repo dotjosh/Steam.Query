b03446220147dcb99fabbf1910aee6b4d0ed44d0
Josh Schwartzberg <dotjosh@gmail.com>
2014-06-29 21:33:47 -0400

    Updates assembly description

--- Source/Steam.Query/Properties/AssemblyInfo.cs
+++ Source/Steam.Query/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Steam.Query")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for querying the Steam Master Server list")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dotjosh Studios, Inc.")]
 [assembly: AssemblyProduct("Steam.Query")]
